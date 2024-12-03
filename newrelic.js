13ac11fd4c4a2db729e4532e3843c8c6dbfdf922
SEP5155 <serg.papusha@gmail.com>
2024-12-03 23:13:39 +0200

    added config for heroku and removed fly

---
+++ newrelic.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.config = {
+    app_name: ['Heroku App'], // Название приложения в New Relic
+    license_key: process.env.NEW_RELIC_LICENSE_KEY, // Лицензионный ключ
+    logging: {
+        level: 'info', // Уровень логирования
+    },
+    distributed_tracing: {
+        enabled: true, // Включение распределенного трейсинга
+    },
+    transaction_tracer: {
+        enabled: true,
+        transaction_threshold: 'apdex_f', // Запись медленных транзакций
+    },
+    error_collector: {
+        enabled: true,
+        ignore_status_codes: [404], // Игнорирование 404-х ошибок
+    },
+};
