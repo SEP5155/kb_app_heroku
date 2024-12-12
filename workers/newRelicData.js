352c62e0e6ab89173f7f63d096c6faa52cd36db8
SEP5155 <serg.papusha@gmail.com>
2024-12-12 23:08:48 +0200

    adjusted NewRelicData

--- workers/newRelicData.js
+++ workers/newRelicData.js
@@ -1,14 +1,16 @@
 const newrelic = require('newrelic');
 
 // 4. Настраиваем логирование памяти
-module.exports = setInterval(() => {
-    const memoryUsage = process.memoryUsage();
-
-    newrelic.recordustomEvent('MemoryUsage', {
-        heapUsed: (memoryUsage.heapUsed / 1024 / 1024).toFixed(2), // MB
-        rss: (memoryUsage.rss / 1024 / 1024).toFixed(2), // MB
-        external: (memoryUsage.external / 1024 / 1024).toFixed(2), // MB
-    });
-
-    console.log('Memory usage metrics sent to New Relic');
-}, 60000);
+module.exports = () => {
+    setInterval(() => {
+        const memoryUsage = process.memoryUsage();
+    
+        newrelic.recordustomEvent('MemoryUsage', {
+            heapUsed: (memoryUsage.heapUsed / 1024 / 1024).toFixed(2), // MB
+            rss: (memoryUsage.rss / 1024 / 1024).toFixed(2), // MB
+            external: (memoryUsage.external / 1024 / 1024).toFixed(2), // MB
+        });
+    
+        console.log('Memory usage metrics sent to New Relic');
+    }, 60000);
+} 
