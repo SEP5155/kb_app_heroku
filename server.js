35ce23ff547d51f5e36dd75553a94d9957c9862a
SEP5155 <serg.papusha@gmail.com>
2024-12-15 19:08:00 +0200

    enabled route observer

--- server.js
+++ server.js
@@ -14,17 +14,17 @@ const app = express();
 const path = require('path');
 
 
-if (process.env.ENVIRONMENT === "production") {
-    // gcObserver();
-    pushMemUseLogs();
-}
+// if (process.env.ENVIRONMENT === "production") {
+//     // gcObserver();
+//     pushMemUseLogs();
+// }
 
 
 dotenv.config({ path: './config.env'});
 
-// const DB = process.env.DATABASE;
-// // const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
-// console.log(DB);
+const DB = process.env.DATABASE;
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+console.log(DB);
 
 mongoose.connect(DB).then( () => console.log('DB connected!')).catch(err => console.error('Connection error:', err));;
 
