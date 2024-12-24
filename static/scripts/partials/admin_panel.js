1a5942491624f009d957761cf5eb9c0d066e0bfa
SEP5155 <serg.papusha@gmail.com>
2024-12-24 15:23:53 +0200

    added some logic to admin panel

--- static/scripts/partials/admin_panel.js
+++ static/scripts/partials/admin_panel.js
@@ -1,7 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const action_type = document.querySelectorAll('.admin_action.type-add');
+    const action_type_add = document.querySelectorAll('.admin_action.type-add');
+    const action_type_edit = document.querySelectorAll('.admin_action.type-edit');
 
-    action_type.forEach(action => {
+    const routes = {
+        action_edit_guide: '/api/v1/guide/',
+        action_edit_response: '/api/v1/responses/'
+    }
+
+    action_type_add.forEach(action => {
         console.log(action.id);
         action.addEventListener('click', (event) => {
             event.stopPropagation();
@@ -16,6 +22,50 @@ document.addEventListener('DOMContentLoaded', () => {
                 guideForm.classList.remove('active');
                 responseForm.classList.add('active');
             }
+        })
+    })
+
+    action_type_edit.forEach(action => {
+        action.addEventListener('click', async function (event) {
+            event.stopPropagation();
+
+            const articlesContainer = document.querySelector('.articles_list');
+
+            const route = routes[action.id]
+
+            const response = await fetch(route, {
+                method: 'GET', // Или 'PATCH' для обновления
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (!response.ok) throw new Error('Failed to get data');
+
+            result = await response.json();
+            console.log(result);
+
+            articlesContainer.innerHTML = '';
+
+            const dataToRender = route === '/api/v1/guide/'
+                ? result.guides.map(item => `<li>${item.technology } - ${item.command}</li>`)
+                : route === '/api/v1/responses/'
+                    ? result.data
+                    .map(item => `<li>${item.topic}</li>`)
+                    : [];
+            articlesContainer.innerHTML = dataToRender.join('');
+
+
+            // if (route === '/api/v1/guide/') {
+            //     articlesContainer.innerHTML = '';
+            //     articlesContainer.innerHTML = result.guides
+            //     .map(item => `<li>${item.technology } - ${item.command}</li>`)
+            //     .join('');
+            // } else if (route === '/api/v1/responses/') {
+            //     articlesContainer.innerHTML = '';
+            //     articlesContainer.innerHTML = result.data
+            //     .map(item => `<li>${item.topic}</li>`)
+            //     .join('');
+            // }
+            
+
         })
     })
 })
