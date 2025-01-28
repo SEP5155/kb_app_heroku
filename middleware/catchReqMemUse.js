dca1b187cf62546d7d85d0bf079d5d8c06512e37
Serhii home acc <serg.papusha@gmail.com>
2025-01-28 21:34:29 +0200

    added some logs

--- middleware/catchReqMemUse.js
+++ middleware/catchReqMemUse.js
@@ -18,7 +18,7 @@ module.exports = (req, res, next) => {
         const parcedUrl = new URL(req.original, `${protocol}://${req.headers.host}`)
         const routePath = parcedUrl.pathname;
         const queryParams = parcedUrl.searchParams.toString() || "";
-        console.log('prorocol' + req.protocol + 'parcedUrl: ' + `${parcedUrl}`);
+        console.log('prorocol ' + req.protocol + 'parcedUrl: ' + `${parcedUrl}, req.headers.host: ` + req.headers.host + 'req.original: ' + req.original);
 
         if (memoryUsagePercentage > MEMORY_THRESHOLD && process.env.USE_MEMORY_LIMIT === "true") {
             // If memory usage is above 50%, don't run middleware
